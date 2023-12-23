1a2d2ac86724cd318c0c1af8ae41818b6240e855
Deepakrocan <sdeepakdeepu332@gmail.com>
2023-12-23 21:31:43 +0530

    add

--- code01.py
+++ code01.py
@@ -1,1 +1,11 @@
-git 
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print('Title:', yt.title)
+print('Views:', yt.views)
+
+yd = yt.streams.get_highest_resolution()
+yd.download('https://www.youtube.com/watch?v=Rgx8dpiPwpA')
