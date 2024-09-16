bcae65440228e17225a6da483f99bd18796b8f66
Deepakrocan <123934788+Deepakrocan@users.noreply.github.com>
2024-09-16 11:15:41 +0530

    String and Variable's
    Example code of String and Varabiles

---
+++ String/s01.py
@@ -0,0 +1,35 @@
+# List of number
+a = [1,2,3,4,5,]
+# i want to call particular number by calling by this code
+print(a[2])
+
+# change a number into list tag  44 to 77 
+a[3] = 11
+print(a)
+
+# i want a particular amount in the list tag
+a[1:3]
+print(a)
+
+# adding a new value in the list tag 
+a.append(4)
+print(a)
+
+# i am extending a more value
+a.extend([6,7,8,9,10])
+print(a)
+
+# removeing a some number in the list 
+a.remove(1)
+print(a)
+
+# Sort the all the interger value
+a.sort()
+print(a)
+
+# using method for reverse the number
+a.reverse()
+print(a)
+
+#finding a length for value
+len(a)
